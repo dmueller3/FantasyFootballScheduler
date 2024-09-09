c75d265523c94b00076aed72a27705429b778539
Derek Mueller <dmueller3@madisoncollege.edu>
2024-09-09 15:16:02 -0500

    put driver in the proper class for logging

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,3 +1,9 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Driver {
+
     private final Logger logger = LogManager.getLogger(this.getClass());
+
+
 }
