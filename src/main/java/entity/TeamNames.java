42ef4ecac195340a9f8ce5a8c532c032ce0db912
Derek Mueller <dmueller3@madisoncollege.edu>
2024-09-21 13:57:31 -0500

    finished the TeamNames class

--- src/main/java/entity/TeamNames.java
+++ src/main/java/entity/TeamNames.java
@@ -1,5 +1,7 @@
 package entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.*;
 
 /**
@@ -8,7 +10,59 @@ import javax.persistence.*;
 @Entity
 @Table(name = "teams")
 public class TeamNames {
+    @Column(name = "team_name")
     private String teamName;
+    @Column
     private String teamOwner;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native")
     private int id;
+
+    /**
+     * Empty constructor to instantiate a new team
+     */
+    public TeamNames() {
+
+    }
+
+    /**
+     * Builds a new team
+     * @param teamName the name of the fantasy football team
+     * @param teamOwner the owner of the fantasy football team
+     */
+    public TeamNames(String teamName, String teamOwner) {
+        this.teamName = teamName;
+        this.teamOwner = teamOwner;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getTeamOwner() {
+        return teamOwner;
+    }
+
+    public void setTeamOwner(String teamOwner) {
+        this.teamOwner = teamOwner;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "TeamNames [teamName=" + teamName + ", teamOwner=" + teamOwner + "]";
+    }
 }
