4b38bf376c897a41058effe8d889a3bfbfc04210
Derek Mueller <dmueller3@madisoncollege.edu>
2024-10-02 20:24:42 -0500

    wrote and successfully passed all tests for TeamDao

--- src/test/java/persistence/TeamDaoTest.java
+++ src/test/java/persistence/TeamDaoTest.java
@@ -28,25 +28,45 @@ public class TeamDaoTest {
 
     @Test
     public void update() {
+        TeamNames team = teamDao.getById(1);
+        team.setTeamName("Patriots");
+        teamDao.update(team);
+
+        TeamNames actualName = teamDao.getById(1);
+        assertEquals("Patriots", actualName.getTeamName());
     }
 
     @Test
     public void insert() {
+        TeamNames team = new TeamNames("New Team", "Billy Bob");
+        int insertedId = teamDao.insert(team);
+        assertNotEquals(0, insertedId);
+        TeamNames insertedTeam = teamDao.getById(insertedId);
+        assertEquals("New Team", insertedTeam.getTeamName());
     }
 
     @Test
     public void delete() {
+        teamDao.delete(teamDao.getById(2));
+        assertNull(teamDao.getById(2));
     }
 
     @Test
     public void getAll() {
+        List<TeamNames> teams = teamDao.getAll();
+        assertEquals(2, teams.size());
     }
 
     @Test
     public void getByPropertyEqual() {
+        List<TeamNames> teams = teamDao.getByPropertyEqual("teamName", "Team 1");
+        assertEquals(1, teams.size());
+        assertEquals(1, teams.get(0).getId());
     }
 
     @Test
     public void getByPropertyLike() {
+        List<TeamNames> teams = teamDao.getByPropertyLike("teamName", "Team");
+        assertEquals(2, teams.size());
     }
 }
