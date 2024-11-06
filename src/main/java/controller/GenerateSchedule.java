851882b3cdfa6d7c7bb890e45e821070de6ecff3
Derek Mueller <dmueller3@madisoncollege.edu>
2024-11-05 18:54:41 -0600

    set method to take team indexed 0 and 1 and add to weekly matchups if it doesn't exist break the criteria

--- src/main/java/controller/GenerateSchedule.java
+++ src/main/java/controller/GenerateSchedule.java
@@ -20,15 +20,34 @@ public class GenerateSchedule {
     public void scheduleCreation() {
         // Loop until the number of weeks scheduled equals the numberOfWeeks
         for (int weekNumber = 1; weekNumber < numberOfWeeks; weekNumber++) {
+            final int TEAMS_PER_MATCHUP = 2;
             List<String> matchupsForWeek = new ArrayList<>();
-            List<String> teamsNotAssigned = new ArrayList<>();
+            List<String> teamsNotAssigned = new ArrayList<>(selectedTeams);
 
             /* Used Hamza Khan's response here for shuffling an arraylist
              * https://stackoverflow.com/questions/16112515/how-to-shuffle-an-arraylist
              */
             Collections.shuffle(teamsNotAssigned);
 
+            while (teamsNotAssigned.size() >= TEAMS_PER_MATCHUP) {
+                String team1 = teamsNotAssigned.get(0);
+                String team2 = teamsNotAssigned.get(1);
+                String matchup = team1 + " vs. " + team2;
 
+                // Check if the matchup is valid based on the history
+                if (matchupValidity(matchup)) {
+                    matchupsForWeek.add(matchup);
+                    teamsNotAssigned.remove(0);
+                    teamsNotAssigned.remove(1);
+                } else {
+                    Collections.shuffle(teamsNotAssigned);
+                }
+            }
+            schedule.add(new ArrayList<>(matchupsForWeek));
         }
+    }
+
+    public boolean matchupValidity(String matchup) {
+
     }
 }
