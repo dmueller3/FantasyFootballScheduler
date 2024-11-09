435ee9dcc1026d4dcb7d3e701a0bbd07814eb941
Derek Mueller <dmueller3@madisoncollege.edu>
2024-11-09 12:25:33 -0600

    handled if there are more than 4 teams, sent redirect to new jsp with schedule

--- src/main/java/controller/EnterTeamNames.java
+++ src/main/java/controller/EnterTeamNames.java
@@ -31,7 +31,27 @@ public class EnterTeamNames extends HttpServlet {
         request.setAttribute("numberOfTeams", numberOfTeams); // change later
         request.setAttribute("teams", teamDao.getAll());
 
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/teamNames.jsp");
+        // Get the values from this form for team names
+        String firstTeam = request.getParameter("firstTeam");
+        String secondTeam = request.getParameter("secondTeam");
+        String thirdTeam = request.getParameter("thirdTeam");
+        String fourthTeam = request.getParameter("fourthTeam");
+
+        // Handle if there are more than 4 teams
+        if (numberOfTeams >= 6) {
+            String fifthTeam = request.getParameter("fifthTeam");
+            String sixthTeam = request.getParameter("sixthTeam");
+        }
+        if (numberOfTeams >= 8) {
+            String seventhTeam = request.getParameter("seventhTeam");
+            String eighthTeam = request.getParameter("eighthTeam");
+        }
+        if (numberOfTeams == 10) {
+            String ninthTeam = request.getParameter("ninthTeam");
+            String tenthTeam = request.getParameter("tenthTeam");
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("scheduleResult.jsp");
         dispatcher.forward(request, response);
     }
 }
---
+++ src/main/webapp/scheduleResult.jsp
@@ -0,0 +1,24 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<c:set var="title" value="Enter Team Names" />
+<%@include file="head.jsp"%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<body>
+<header>
+  <h1>Fantasy Football Scheduler</h1>
+</header>
+<main>
+  <h3>Enter Team Names</h3>
+  <form action="${pageContext.request.contextPath}/enterNames" method="POST">
+    <c:choose>
+      <c:when test="${numberOfTeams} = 6">
+
+      </c:when>
+    </c:choose>
+  </form>
+
+  <p>Don't see a team name? </p><a href="">Add a team</a>
+</main>
+</body>
+</html>
+
